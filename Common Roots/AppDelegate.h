113aa2ba8a087e4ec97441eb09da4bddb57490a1
Spencer Yen <spencersyen@gmail.com>
2015-01-17 16:51:12 -0800

    Initial Commit

---
+++ Common Roots/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Common Roots
+//
+//  Created by Spencer Yen on 1/17/15.
+//  Copyright (c) 2015 Parameter Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Common Roots/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Common Roots
+//
+//  Created by Spencer Yen on 1/17/15.
+//  Copyright (c) 2015 Parameter Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
